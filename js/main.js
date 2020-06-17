8b23217a7e3b0063332d2d003a9ca7f4cfecb524
Maria Popleteeva <m.popleteeva@gmail.com>
2020-06-17 11:50:35 +0300

    генерация карточек

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 var titles = ["Гнездышко", "Уютное убежище", "Твой рай"];
 var hotelTypes = ["palace", "flat", "house", "bungalo"];
 var times = ["12:00", "13:00", "14:00"];
-var descriptions = ["Очень красиво!", "Очень хорошо!", "Очень чисто!"];
+var description = ["Очень красиво!", "Очень хорошо!", "Очень чисто!"];
 var images = [
   "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
   "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
@@ -12,14 +12,16 @@ var images = [
 var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 var PIN_IMG_WIDTH = 40;
 var PIN_IMG_HEIGHT = 40;
+var PHOTO_IMG_WIDTH = 45;
+var PHOTO_IMG_HEIGHT = 40;
 
 var getRandomNumber = function (min, max) {
-  return min + Math.ceil(Math.random() * (max - min));
+  return min + Math.floor(Math.random() * (max - min));
 };
 
 var getDifMassive = function (pool) {
   var myPool = pool.slice();
-  myPool.length = Math.floor(Math.random() * (myPool.length - 1)) + 1;
+  myPool.length = Math.floor(Math.random() * myPool.length) + 1;
   return myPool;
 };
 
@@ -78,3 +80,81 @@ mapPins.appendChild(createPins(hotels));
 
 var map = document.documentElement.querySelector(".map");
 map.classList.remove("map--faded");
+
+var cardTemplate = document.querySelector("#card").content;
+
+function createCards(array) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < array.length; i++) {
+    var element = cardTemplate.cloneNode(true);
+    var card = element;
+    card.querySelector(".popup__avatar").src = array[i].author.avatar;
+    card.querySelector(".popup__title").textContent = array[i].offer.title;
+    card.querySelector(".popup__text--address").textContent = array[i].offer.address;
+    card.querySelector(".popup__text--price").textContent = array[i].offer.price + "₽/ночь";
+
+    var type = "Квартира";
+    if (array[i].offer.type === "bungalo") {
+      type = "Бунгало";
+    }
+    if (array[i].offer.type === "house") {
+      type = "Дом";
+    }
+    if (array[i].offer.type === "palace") {
+      type = "Дворец";
+    }
+
+    card.querySelector(".popup__type").textContent = type;
+
+    array[i].offer.type;
+    card.querySelector(".popup__text--capacity").textContent =
+      array[i].offer.rooms +
+      " " +
+      "комнаты для" +
+      " " +
+      array[i].offer.guests +
+      " " +
+      "гостей";
+    card.querySelector(".popup__text--time").textContent =
+      "Заезд после" +
+      " " +
+      array[i].offer.checkin +
+      ", выезд до" +
+      " " +
+      array[i].offer.checkout;
+    card.querySelector(".popup__feature--wifi").textContent = array[i].offer.features[1];
+    card.querySelector(".popup__feature--dishwasher").textContent = array[i].offer.features[2];
+    card.querySelector(".popup__feature--parking").textContent = array[i].offer.features[3];
+    card.querySelector(".popup__feature--washer").textContent = array[i].offer.features[4];
+    card.querySelector(".popup__feature--elevator").textContent = array[i].offer.features[5];
+    card.querySelector(".popup__feature--conditioner").textContent =
+      array[i].offer.features[6];
+    card.querySelector(".popup__description").textContent = array[i].offer.description;
+
+    var cardPhotos = card.querySelector(".popup__photos");
+    cardPhotos.innerHTML = "";
+
+    var createPhotos = function (array) {
+      var fragment = document.createDocumentFragment();
+
+      for (var j = 0; j < array.length; j++) {
+        var newImg = document.createElement("img");
+        newImg.width = PHOTO_IMG_WIDTH;
+        newImg.height = PHOTO_IMG_HEIGHT;
+        newImg.src = array[j];
+        newImg.alt = "Фотография жилья";
+
+        fragment.appendChild(newImg);
+      }
+      return fragment;
+    };
+    cardPhotos.appendChild(createPhotos(array[i].offer.photos));
+    card.querySelectorAll("li:empty").forEach((element) => element.remove());
+    fragment.appendChild(card);
+  }
+  return fragment;
+}
+
+var before = document.querySelector(".map__filters-container");
+var parentDiv = before.parentNode;
+parentDiv.insertBefore(createCards(hotels), before);
